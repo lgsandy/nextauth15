e719147e92844650a8879137a3f8d75f04c16f51
santosh.k <santosh.k@apollodart.com>
2025-02-26 18:02:36 +0530

    Form is implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,77 @@
+"use client";
+import CustomCard from "@/components/shared/custom-card";
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(2).max(8),
+});
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div className="flex px-2 py-2">
+    <div className="flex justify-center items-center px-2 py-2 h-screen">
       <Button>Test</Button>
+      <div className="w-1/6">
+        <CustomCard title="Login ">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CustomCard>
+      </div>
     </div>
   );
 }
---
+++ components/shared/custom-card.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface CardProp {
+  title?: string;
+  description?: string;
+  children?: React.ReactNode;
+  footer?: React.ReactNode;
+}
+export default function CustomCard({
+  title,
+  description,
+  children,
+  footer,
+}: CardProp) {
+  return (
+    <Card>
+      {(title || description) && (
+        <CardHeader>
+          {title && <CardTitle>{title}</CardTitle>}
+          {description && <CardDescription>{description}</CardDescription>}
+        </CardHeader>
+      )}
+
+      <CardContent>
+        <p>{children}</p>
+      </CardContent>
+      {footer && (
+        <CardFooter>
+          <p>{footer}</p>
+        </CardFooter>
+      )}
+    </Card>
+  );
+}
