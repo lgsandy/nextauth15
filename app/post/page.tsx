c07a5b607cca43b2a59c497a10a8bbec95605544
santosh.k <santosh.k@apollodart.com>
2025-02-26 16:10:02 +0530

    Data table is implemented

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -4,9 +4,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { DataTable } from "./data-table";
 import { User, getColumns } from "./columns";
+import { Button } from "@/components/ui/button";
 
 export default function Post() {
   const [posts, setPost] = useState<User[]>([]);
+  const [isAdd, setIsAdd] = useState<boolean>(false);
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(({ data }) => {
       setPost(data);
@@ -19,9 +22,16 @@ export default function Post() {
   const handleDeleteClick = (id: number) => {
     console.log("Delete Clicked:", id);
   };
-
+  const addNew = () => {
+    setIsAdd(true);
+  };
   return (
     <div className="container mx-auto py-10">
+      <div className="flex justify-end mb-2">
+        <Button onClick={addNew} size={"sm"}>
+          Add
+        </Button>
+      </div>
       <DataTable
         columns={getColumns(handleEditClick, handleDeleteClick)}
         data={posts}
